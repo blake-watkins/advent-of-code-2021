37a1c035bcd0fd81b6a3309edc4f2f3b0c61acf4
Blake Watkins <blakewatkins@gmail.com>
2021-12-01 09:27:52 +1300

    Initialize 2021 repository.

---
+++ package.lisp
@@ -0,0 +1,6 @@
+(defpackage :advent-of-code-2021
+  (:nicknames :aoc-2021)
+  (:use :cl :aoc :iterate)
+  (:shadowing-import-from :fset :reverse :size :convert :set :set-difference :seq :subseq)
+  (:import-from :fset :concat :empty-set :empty-map :domain-contains? :lookup :contains? :includef :arb))
+
