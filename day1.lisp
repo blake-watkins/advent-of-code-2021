27691f958252c91d93ee689cfadbcdaffa2ddfcd
Blake Watkins <blakewatkins@gmail.com>
2021-12-01 18:38:45 +1300

    Finish day 1.

---
+++ day1.lisp
@@ -0,0 +1,28 @@
+(in-package :aoc-2021)
+
+(defun parse-file ()
+ (parse-list (parse-number) (parse-newline)) )
+
+(defun sum-between (nums a b)
+  (apply #'+ (subseq nums a b)))
+
+(defun day1 (input)
+  (let ((parsed (run-parser (parse-file) input)))
+    (iter
+      (with part1 = 0)
+      (with part2 = 0)
+      
+      (for idx from 0)
+      (for num in parsed)
+      
+      (when (>= idx 1)
+	(when (> (elt parsed idx)
+		 (elt parsed (1- idx)))
+	  (incf part1)))
+
+      (when (>= idx 3)
+	(when (> (sum-between parsed (- idx 2) (1+ idx))
+		 (sum-between parsed (- idx 3) idx))
+	  (incf part2)))
+      
+      (finally (return (list  part1 part2))))))
