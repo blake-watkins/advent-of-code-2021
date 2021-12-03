26d59bd3040b03c5235186f0010874e929282cfc
Blake Watkins <blakewatkins@gmail.com>
2021-12-04 05:44:38 +1300

    Remove redundant rating function.

--- day3.lisp
+++ day3.lisp
@@ -3,34 +3,36 @@
 (defun parse-file ()
   (parse-lines (parse-list (parse-digit) "")))
 
-(defun most-common (numbers on-eq)
+(defun most-common (numbers)
   (let* ((half-length (/ (length numbers) 2))
          (one-counts (reduce (lambda (a b) (map 'list #'+ a b)) numbers))
          (most-common (mapcar (lambda (c) (cond
-                                            ((= c half-length) on-eq)
+                                            ((= c half-length) :eq)
                                             ((> c half-length) 1)
                                             (t 0)))
                               one-counts)))
     most-common))
 
-(defun remove-at (position value numbers)
-  (remove-if (lambda (word) (= (elt word position) value)) numbers))
+(defun keep-if (position value numbers)
+  (remove-if-not (lambda (word) (= (elt word position) value)) numbers))
 
-(defun get-oxygen-rating (position numbers)
+(defun get-rating (position numbers &key (oxygen-p t))
   (if (= 1 (length numbers))
       (car numbers)
-      (let ((most-common (elt (most-common numbers 1) position)))
-        (get-oxygen-rating (1+ position)
-                           (remove-at position (- 1 most-common) numbers)))))
-
-(defun get-co2-rating (position numbers)
-  (if (= 1 (length numbers))
-      (car numbers)
-      (let ((most-common (elt (most-common numbers 1) position)))
-        (get-co2-rating (1+ position)
-                        (remove-at position most-common numbers)))))
+      (let ((most-common (elt (most-common numbers) position)))
+        (case most-common
+          (:eq (get-rating (1+ position)
+                           (keep-if position (if oxygen-p 1 0) numbers)
+                           :oxygen-p oxygen-p))
+          (t (get-rating (1+ position)
+                         (keep-if position
+                                  (if oxygen-p
+                                      most-common
+                                      (- 1 most-common))
+                                  numbers)
+                         :oxygen-p oxygen-p))))))
 
 (defun day3 (input)
   (let* ((parsed (run-parser (parse-file) input)))
-    (* (digits-to-int (get-oxygen-rating 0 parsed))
-       (digits-to-int (get-co2-rating 0 parsed)))))
+    (* (digits-to-int (get-rating 0 parsed))
+       (digits-to-int (get-rating 0 parsed :oxygen-p nil)))))
