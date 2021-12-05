285b5da9550c57de92f6a978a486b674845b8117
Blake Watkins <blakewatkins@gmail.com>
2021-12-06 08:42:54 +1300

    Finish and tidy up day 5.

---
+++ day5.lisp
@@ -0,0 +1,25 @@
+(in-package :aoc-2021)
+
+(defun parse-file ()
+  (parse-lines (with-monad
+                 (assign a (parse-number-list))
+                 (parse-string " -> ")
+                 (assign b (parse-number-list))
+                 (unit (list a b)))))
+
+(defun day5 (input &optional (part2 nil))
+  (let ((parsed (run-parser (parse-file) input))
+	(counts (make-hash-table :test 'equal)))
+    (iter
+      (for (a b) in parsed)
+      (for diff = (point- b a))
+      (for dir = (point-signum diff))
+      (for steps = (1+ (apply #'max (point-abs diff))))
+      (when (or part2 (find 0 diff))
+	(iter
+	  (repeat steps)
+	  (for pos first a then (point+ pos dir))
+	  (incf (gethash pos counts 0)))))
+    (iter
+      (for (nil v) in-hashtable counts)
+      (counting (>= v 2)))))
