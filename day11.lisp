38156758391ec20364d06da83d6589174d2b5138
Blake Watkins <blakewatkins@gmail.com>
2021-12-12 16:53:22 +1300

    Rewrite to use state, not sure if it's any better.

--- day11.lisp
+++ day11.lisp
@@ -5,53 +5,86 @@
 
 (defun neighbours (p map)
   (remove-if-not
-   (lambda (p) (gethash p map))
+   (lambda (p) (fset:domain-contains? map p))
    (mapcar (lambda (d) (map 'list #'+  p d))
            '((1 0) (-1 0) (0 1) (0 -1) (1 1) (-1 1) (1 -1) (-1 -1)))))
 
-(defun increase (map)
-  (iter
-    (with ret = (make-hash-table :test 'equal))
-    (with to-flash = (fset:empty-seq))
-    (for (octopus brightness) in-hashtable map)
-    (setf (gethash octopus ret) (1+ brightness))
-    (when (> (1+ brightness) 9)
-      (fset:push-last to-flash octopus))
-    (finally (return (list ret to-flash)))))
+(def-state (map flashed flashes))
 
-(defun flash (map to-flash)
-  (iter
-    (with flashed = (fset:empty-set))
-    (while (> (fset:size to-flash) 0))
-    (for flashing = (fset:pop-first to-flash))
-    (when (not (fset:contains? flashed flashing))
-      (mapc (lambda (p)
-              (let ((new (1+ (gethash p map))))
-                (setf (gethash p map) new)
-                (when (> new 9)
-                  (fset:push-last to-flash p))))
-            (neighbours flashing map)))
-    (fset:includef flashed flashing)
-    (finally
-     (iter
-       (for flashed-o in-fset flashed)
-       (setf (gethash flashed-o map) 0))
-     (return (list map (fset:size flashed))))))
+(defun get-neighbours (octopus)
+  (with-monad
+    (assign map (get-map))
+    (unit (neighbours octopus map))))
 
-(defun day11 (input)
+(defun set-brightness (octopus op)
+  "Either increment octopus brightness if OP is :INC, or set to zero if OP is :RESET. Returns new brightness. "
+  (with-monad
+    (assign map (get-map))
+    (let ((brightness (ecase op
+                        (:reset 0)
+                        (:inc (1+ (fset:lookup map octopus))))))
+      (with-monad
+        (set-map (fset:with map octopus brightness))
+        (unit brightness)))))
+
+(defun reset-flashed ()
+  "Reset the brightness of flashed octopuses. Clear the set of flashed octopuses. Increase and return the count of flashes. "
+  (with-monad
+    (assign flashed (get-flashed))
+    (mapfsetm_ (lambda (octopus)
+                 (set-brightness octopus :reset))
+              flashed)
+    (set-flashed (fset:empty-set))
+
+    (assign flashes (get-flashes))
+    (let ((num-flashed (fset:size flashed)))
+      (with-monad
+        (set-flashes (+ flashes num-flashed))
+        (unit num-flashed)))))
+
+(defun increase (octopus)
+  "Increase octopus brightness. Flash the octopus if it's more than 9. "
+  (with-monad
+    (assign brightness (set-brightness octopus :inc))
+    (whenm (> brightness 9)
+      (flash octopus))))
+
+(defun flash (octopus)
+  "If the octopus hasn't already flashed, flash it and increase its neighbours. "
+  (with-monad
+    (assign flashed (get-flashed))
+    (whenm (not (fset:contains? flashed octopus))
+      (with-monad
+        (set-flashed (fset:with flashed octopus))
+        (assign neighbours (get-neighbours octopus))
+        (mapm_ #'increase neighbours)))))
+
+(defun step-octopuses ()
+  "Increase all octopuses, flashing when necessary. Reset flashed. Return number of flashed. "
+  (with-monad
+    (assign map (get-map))
+    (mapfsetm_ #'increase map)
+    (reset-flashed)))
+
+(defun step-octopuses-until (steps flashes-target)
+  "Step octopuses until flashes-target octopuses flash. Return number of steps. "
+  (with-monad
+    (assign flashes (step-octopuses))
+    (if (= flashes flashes-target)
+        (unit (1+ steps))
+        (step-octopuses-until (1+ steps) flashes-target))))
+
+(defun day11 (input &optional (part1 nil))
   (let* ((parsed (run-parser (parse-file) input))
-         (ht (iter
-               (with ret = (make-hash-table :test 'equal))
-               (for r below (length parsed))
-               (iter
-                 (for c below (length (first parsed)))
-                 (setf (gethash (list r c) ret) (elt (elt parsed r) c)))
-               (finally (return ret)))))
-    (iter
-      (with map = ht)
-      (for num-steps from 1)
-      (for (new-map to-flash) = (increase map))
-      (for (new-map2 flashes) = (flash new-map to-flash))      
-      (setf map new-map2)
-      (until (= (hash-table-count map) flashes))
-      (finally (return num-steps)))))
+         (map (map-from-list-list parsed)))
+    (run-state
+     (if part1
+         (with-monad
+           (n-of 100 (step-octopuses))
+           (get-flashes))
+         (step-octopuses-until 0 (fset:size map)))
+     (make-state :map map
+                 :flashed (fset:empty-set)
+                 :flashes 0))))
+
+
