a335aaf02d8a1c149e2548043daf0ddf9a9c6abe
Blake Watkins <blakewatkins@gmail.com>
2021-12-15 18:59:48 +1300

    Finish day 15

--- day15.lisp
+++ day15.lisp
@@ -3,42 +3,30 @@
 (defun parse-file ()
   (parse-lines (parse-list (parse-digit) "")))
 
-(defun neighbours (p map)
-  (remove-if-not (lambda (p) (and (<= 0 (first p)  (1- (length map)))
-				  (<= 0 (second p) (1- (length (first map))))))
-             (mapcar (lambda (d) (map 'list #'+ d p))
-                     '((1 0) (0 1) (-1 0) (0 -1)))))
+(defun neighbours (p max)
+  (remove-if-not
+   (lambda (neighbour)
+     (every (lambda (c m) (<= 0 c (1- m))) neighbour max))
+   (mapcar (lambda (d) (map 'list #'+ d p))
+	   '((1 0) (0 1) (-1 0) (0 -1)))))
 
 (defun risk (cur map)
-  (elt (elt map (first cur)) (second cur)))
+  (multiple-value-bind (rq rr) (floor (first cur) (length map))
+    (multiple-value-bind (cq cr) (floor (second cur) (length (first map)))
+      (let ((base-risk (elt (elt map rr) cr)))
+	(1+ (mod (+ (1- base-risk) rq cq) 9))))))
 
-(defun total-risk (cur map)
-  (let ((cache (fset:empty-map)))
-    (labels ((total-risk-rec (cur map)
-               (cond
-                 ((fset:domain-contains? cache cur)
-                  (fset:lookup cache cur))
-                 ((null (neighbours cur map)) (risk cur map))
-                 (t 
-                  (let ((ret
-                          (+ (reduce #'min
-                                     (mapcar
-                                      (lambda (n) (total-risk-rec n map))
-                                      (neighbours cur map)))
-                             (risk cur map))))
-                    (fset:includef cache cur ret)
-                    ret)))))
-      (total-risk-rec cur map))))
-
-
-(defun day15 (input)
-  (let ((map (run-parser (parse-file) input))
-        (ret (fset:empty-map)))
+(defun day15 (input &key (part 2))
+  (let* ((map (run-parser (parse-file) input))
+         (ret (fset:empty-map))
+	 (dim (list (length map) (length (first map)))))
+    (when (= part 2)
+      (setf dim (mapcar (lambda (x) (* 5 x)) dim)))
     (dijkstra '(0 0)
               (lambda (vertex parent distance)
 		(declare (ignore parent))
                 (fset:includef ret vertex distance))
               (lambda (v)
                 (mapcar (lambda (n) (list n (risk n map)))
-                        (neighbours v map))))
-    ret))
+                        (neighbours v dim))))
+    (fset:lookup ret (mapcar #'1- dim))))
