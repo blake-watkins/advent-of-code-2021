df32e87110fc0feb624d1ae36078cad3ac0644e2
Blake Watkins <blakewatkins@gmail.com>
2021-12-20 15:57:43 +1300

    Commented. Added WITH-ZIPPER.

--- day18.lisp
+++ day18.lisp
@@ -10,48 +10,71 @@
             (unit (list x y)))
           (parse-number)))
 
-;; find-explosion-site :: Tree a -> Maybe Zipper a
-(defun find-explosion-site (tree)
-  (find-in-tree tree (lambda (tree-node depth)
-		       (and (listp tree-node) (= 4 depth)))))
+;; Zippers
+;; http://learnyouahaskell.com/zippers
+;;
+;; Maybe monad
 
-(defun find-split-site (tree)
-  (find-in-tree tree (lambda (tree-node depth)
-		       (declare (ignore depth))
-		       (and (numberp tree-node) (>= tree-node 10)))))
-
-
+;; Given a function f :: Zipper -> Maybe Zipper, this will return a function
+;; (dont-fail f) :: Zipper -> Maybe Zipper that will not be :nothing. If f
+;; would have failed, then (dont-fail f) will return the zipper originally
+;; passed to it, otherwise it will return f's result.
 (defun dont-fail (f)
   (lambda (zipper)
     (maybe-mplus
      (funcall f zipper)
      (unit zipper))))
 
-;; add-previous :: Int -> Zipper a -> Maybe Zipper a
-(defun add-previous (n)
-  (dont-fail
-   (concatm (go-prev)
-	    (modify (lambda (x) (+ x n)))
-	    (go-next))))
+;; add-to-previous :: Int -> Zipper a -> Maybe Zipper a
+;; Takes an Int and returns a function taking a Zipper -> Maybe Zipper. When
+;; the function is given a zipper it attempts to go to the previous, add the
+;; int, then come back. If anything fails this function will return the
+;; unaltered zipper, otherwise it will return the altered one.
+(defun add-to-previous (n)
+  (dont-fail (lambda (explosion-site)
+	       (with-zipper explosion-site
+		 (go-prev)
+		 (modify (lambda (x) (+ x n)))
+		 (go-next)))))
 
-(defun add-next (n)
-  (dont-fail
-   (concatm (go-next)
-	    (modify (lambda (x) (+ x n)))
-	    (go-prev))))
+(defun add-to-next (n)
+  (dont-fail (lambda (explosion-site)
+	       (with-zipper explosion-site
+		 (go-next)
+		 (modify (lambda (x) (+ x n)))
+		 (go-prev)))))
 
-;; explode :: Tree -> Maybe Tree
+;; find-explosion-site :: Tree a -> Maybe Zipper a
+;; Uses utility fun FIND-IN-TREE that returns a zipper to the first (in-order)
+;; node that satisfies the predicate. Returns :nothing if there aren't any
+;; explosion sites.
+;; The explosion site must be a pair (list) at depth 4.
+(defun find-explosion-site (tree)
+  (find-in-tree tree
+		(lambda (tree-node depth)
+		  (and (listp tree-node) (= 4 depth)))))
+
+;; Takes a tree, returns a Maybe Tree. A tree is returned if there was an
+;; explosion, otherwise :nothing is returned.
 (defun explode (tree)
   (with-monad
     (assign explosion-site (find-explosion-site tree))
     (assign ret (let ((pair (zipper-tree explosion-site)))
-		  (funcall (concatm 
-			    (attach 0)
-			    (add-previous (first pair))
-			    (add-next (second pair)))
-			   explosion-site)))
+		  (with-zipper explosion-site
+		    (attach 0)
+		    (add-to-previous (first pair))
+		    (add-to-next (second pair)))))
     (unit (zipper-to-tree ret))))
 
+;; A split site is a single number that is >= 10. 
+(defun find-split-site (tree)
+  (find-in-tree tree
+		(lambda (tree-node depth)
+		  (declare (ignore depth))
+		  (and (numberp tree-node) (>= tree-node 10)))))
+
+;; Split takes a tree and returns a Maybe Tree. If there was a split the new
+;; tree is returned, otherwise :nothing is returned. 
 (defun split (tree)
   (with-monad
     (assign split-site (find-split-site tree))
@@ -61,31 +84,33 @@
 		  (funcall (attach new-pair) split-site)))
     (unit (zipper-to-tree ret))))
 
-(defun reduce-snailnum-fully (snail-num)
+;; Fully reduces snailnum by exploding and splitting. 
+(defun snailnum-reduce (snailnum)
   (iter
-    (for exploded = (from-maybe (explode snail-num)))
+    (for exploded = (from-maybe (explode snailnum)))
     (when exploded
-      (setf snail-num exploded)
+      (setf snailnum exploded)
       (next-iteration))
-    (for split = (from-maybe (split snail-num)))
+    (for split = (from-maybe (split snailnum)))
     (when split
-      (setf snail-num split)
+      (setf snailnum split)
       (next-iteration))
     (while (or exploded split))
-    (finally (return snail-num))))
+    (finally (return snailnum))))
 
-(defun add-snailnums (acc v)
-  (reduce-snailnum-fully (list acc v)))
+;; Add two snailnums. Make a list and then reduce it.
+(defun snailnum+ (a b)
+  (snailnum-reduce (list a b)))
 
-(defun magnitude (snailnum)
+(defun snailnum-magnitude (snailnum)
   (cond
     ((numberp snailnum) snailnum)
-    (t (+ (* 3 (magnitude (first snailnum)))
-	  (* 2 (magnitude (second snailnum)))))))
+    (t (+ (* 3 (snailnum-magnitude (first snailnum)))
+	  (* 2 (snailnum-magnitude (second snailnum)))))))
 
 (defun day18 (input)
   (let ((parsed (run-parser (parse-lines (parse-pair)) input)))
-    (magnitude (reduce #'add-snailnums parsed))))
+    (snailnum-magnitude (reduce #'snailnum+ parsed))))
 
 (defun day18-2 (input)
   (let ((parsed (run-parser (parse-lines (parse-pair)) input)))
@@ -95,6 +120,6 @@
 	(for b in parsed)
 	(unless (equal a b))
 	(in outer (maximizing
-		   (max (magnitude (add-snailnums a b))
-			(magnitude (add-snailnums b a)))))))))
+		   (max (snailnum-magnitude (snailnum+ a b))
+			(snailnum-magnitude (snailnum+ b a)))))))))
 
